bfca31e983fb7107842ad91f6d458d2f476d6b86
Fernando Cruz <fernandocruzrj21@gmail.com>
2018-07-16 20:32:41 -0300

    Initial Commit

---
+++ ChuckNorrisDomain/ChuckNorrisDomain.h
@@ -0,0 +1,19 @@
+//
+//  ChuckNorrisDomain.h
+//  ChuckNorrisDomain
+//
+//  Created by Fernando Cruz on 16/07/18.
+//  Copyright © 2018 Fernando Cruz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ChuckNorrisDomain.
+FOUNDATION_EXPORT double ChuckNorrisDomainVersionNumber;
+
+//! Project version string for ChuckNorrisDomain.
+FOUNDATION_EXPORT const unsigned char ChuckNorrisDomainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChuckNorrisDomain/PublicHeader.h>
+
+
